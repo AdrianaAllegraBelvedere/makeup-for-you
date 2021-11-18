622dedd83f5523b6d95c7dca7dad29eda37dfaf5
Adriana Allegra <adriana.allegrabelvedere@gmail.com>
2021-11-17 23:01:53 -0300

    feat productos

---
+++ js/main.js
@@ -0,0 +1,15 @@
+const producto1 = new Producto('Labial Rojo', 950, 200, "img/producto1.jpg", true);
+const producto2 = new Producto('Delineador', 7000, 100, "img/producto2.jpg", false);
+const producto3 = new Producto('Sombras', 1200, 200, "img/producto3.jpg", true);
+const producto4 = new Producto('Iluminador', 600, 50, "img/producto4.jpg", false);
+const producto5 = new Producto('Rimel', 950, 70, "img/producto5.jpg", false);
+const producto6 = new Producto('Agua Micelar', 1300, 40, "img/producto6.jpg", false);
+const producto7 = new Producto('Serum Reparador', 750, 48, "img/producto7.jpg", false);
+const producto8 = new Producto('Roller de jade', 600, 50, "img/producto8.jpg", true); 
+const producto9 = new Producto('Crema hidratante de manos', 800, 60, "img/producto9.jpg", false);
+const producto10 = new Producto('Base de maquillaje', 1400, 35, "img/producto10.jpg", false);
+const producto11 = new Producto('Kit 4 labiales', 1650, 20, "img/producto11.jpg", true);
+const producto12 = new Producto('kit de sombras', 2300, 30, "img/producto12.jpg", false);
+
+const productos = [producto1, producto2, producto3, producto4, producto5, producto6, producto7, producto8, producto9, producto10, producto11, producto12];
+let carrito =[];
